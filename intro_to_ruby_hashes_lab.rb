0bd99ae8d756a18f1689e2fd53e55ffb501a2422
chic-page-6502 <zacharytq@gmail.com>
2019-07-17 01:24:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,35 +11,7 @@ end
 def monopoly
 	monopoly =  {
 	  :railroads=>
-	  {
-	    :pieces=>4,
-	    :rent_in_dollars=>
-	    {
-	      :one_piece_owned=>25,
-	      :two_pieces_owned=>50,
-	      :three_pieces_owned=>100,
-	      :four_pieces_owned=>200
-	    },
-	    :names=>
-	    {
-	      :reading_railroad=>
-	      {
-	        "mortgage_value"=>"$100"
-	      },
-	      :pennsylvania_railroad=>
-	      {
-	        "mortgage_value"=>"$200"
-	      },
-	      :b_and_o_railroad=>
-	      {
-	        "mortgage_value"=>"$400"
-	      },
-	      :shortline=>
-	      {
-	        "mortgage_value"=>"$800"
-	      }
-	    }
-	  }
+	  {}
 	}
 
 end
@@ -121,8 +93,32 @@ def monopoly_with_fourth_tier
 	monopoly =  {
 	  :railroads=>
 	  {
-
+	    :pieces=>4,
+	    :rent_in_dollars=>
+	    {
+	      :one_piece_owned=>25,
+	      :two_pieces_owned=>50,
+	      :three_pieces_owned=>100,
+	      :four_pieces_owned=>200
+	    },
+	    :names=>
+	    {
+	      :reading_railroad=>
+	      {
+	        "mortgage_value"=>"$100"
+	      },
+	      :pennsylvania_railroad=>
+	      {
+	        "mortgage_value"=>"$200"
+	      },
+	      :b_and_o_railroad=>
+	      {
+	        "mortgage_value"=>"$400"
+	      },
+	      :shortline=>
+	      {
+	        "mortgage_value"=>"$800"
+	      }
 	    }
 	  }
-	
-end
+	}
