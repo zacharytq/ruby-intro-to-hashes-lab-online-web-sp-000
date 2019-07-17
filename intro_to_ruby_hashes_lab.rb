af02762279054d26632bd265d26a05add6d0a22a
chic-page-6502 <zacharytq@gmail.com>
2019-07-17 01:28:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,13 +22,14 @@ def monopoly_with_second_tier
 	  :railroads=>
 	  {
 	    :pieces=>4,
+			:names=>
+	    {
+
+	    }
 	    :rent_in_dollars=>
 	    {
 	    },
-	    :names=>
-	    {
-	      
-	    }
+
 	  }
 	}
 end
